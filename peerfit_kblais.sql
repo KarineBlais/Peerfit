cd8a3dd37e8f4f7406ec4304a45efcb2f7aaf11d
KarineBlais <kabla203@hotmail.com>
2018-09-01 02:08:02 -0400

    First Commit

---
+++ peerfit_kblais.sql
@@ -0,0 +1,55 @@
+/*#1
+Across all reservation partners for January & February, 
+how many completed reservation occured?*/
+
+/*#1 - Answer
+*/
+
+/*#2
+Which studio has the highest rate of reservation abandonment  
+(did not cancel but did not check-in)?*/
+
+/*#2 - Answer
+*/
+
+/*#3
+Which fitness area (i.e., tag) has the highest number of completed 
+reservations for February*/
+
+/*#3 - Answer
+*/
+
+/*#4
+How many members completed at least 1 reservation and no more than 
+1 cancelled reservation in January*/
+
+/*#4 - Answer
+*/
+
+/*#5
+At what time of day do most users book classes? Attend classes?
+(Morning =7-11 AM, Afternoon = 12-4 PM, Evening = 5-10 PM)*/
+
+/*#5 - Answer
+*/
+
+/*#6
+How many confirmed completed reservations did the member (ID) with 
+the most reserved classes in February have*/
+
+/*#6 - Answer
+*/
+
+/*#7
+Write a query that unions the 'mindbody reservations' table
+and clubready-reservations' table*/
+
+/*#8
+What opportunities do you see to improve data storage and 
+standardization for these datasets?*/
+
+/*#9
+What forecasting opportunities do you see with a dataset like this and why?*/
+
+/*#10
+What other data would you propose we gather to make reporting/forecasting more robust and why?*/
